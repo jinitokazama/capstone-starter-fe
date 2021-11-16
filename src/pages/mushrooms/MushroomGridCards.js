0159228d2ad8b1cdf5df650b1f52c76038417354
n0249077 <craig.norton@libertymutual.com>
2021-11-16 11:46:04 -0500

    Updates to mushroom

--- src/pages/mushrooms/MushroomGridCards.js
+++ src/pages/mushrooms/MushroomGridCards.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MushroomDataService from '../../service/MushroomDataService';
 import isAuthenticated from '../../utils/isAuthenticated';
+import "../mushrooms/Mushroom.css"
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -22,7 +23,7 @@ class MushroomGridCards extends React.Component {
             this.setState({ mushroomData: response.data })
         })
             .catch((error) => {
-                console.log("Error: Could not find mushrooms" )
+                console.log("Error: Could not find mushrooms")
             })
     }
 
@@ -39,23 +40,22 @@ class MushroomGridCards extends React.Component {
             <div className="GridCards container mb-3">
                 <Row xs={1} lg={3} className="g-4">
                     {this.state.mushroomData.map((mushroom, idx) => (
-                        <Col key={ idx }>
+                        <Col key={idx}>
                             <Card>
-                                <Card.Img variant="top" src={ mushroom.pictureUrl }/>
+                                <Card.Img variant="top" src={mushroom.pictureUrl} />
                                 <Card.Body>
-                                    <Card.Title>{ mushroom.commonName[0] }</Card.Title>
-                                    <Card.Text>Scientific Name: { mushroom.scientificName }</Card.Text>
-                                    <Card.Text>Description: { mushroom.description }</Card.Text>
-                                    <Card.Text>Color: { mushroom.color }</Card.Text>
-                                    <Card.Text>Characteristics: { mushroom.idCharacteristics }</Card.Text>
-                                    <Card.Text>Edible: { mushroom.edibility }</Card.Text>
-                                    <Card.Text>Toxic: { mushroom.toxicity }</Card.Text>
-                                    <Card.Text>Habitat: { mushroom.habitat }</Card.Text>
-                                    <Card.Text>Location: { mushroom.locations }</Card.Text>
-                                    <Card.Text>Expert: { mushroom.experts }</Card.Text>
-                                    {
-                                      isAuthenticated() ? <Button onClick={() => this.deleteMushrooms(mushroom._id)} >Delete</Button> : null
-                                    }
+                                    <Card.Title>{mushroom.commonName[0]}</Card.Title>
+                                    <Card.Text>Scientific Name: {mushroom.scientificName}</Card.Text>
+                                    <Card.Text>Description: {mushroom.description}</Card.Text>
+                                    <Card.Text>Color: {mushroom.color}</Card.Text>
+                                    <Card.Text>Characteristics: {mushroom.idCharacteristics}</Card.Text>
+                                    <Card.Text>Edible: {mushroom.edibility}</Card.Text>
+                                    <Card.Text>Toxic: {mushroom.toxicity}</Card.Text>
+                                    <Card.Text>Habitat: {mushroom.habitat}</Card.Text>
+                                    <Card.Text>Location: {mushroom.locations}</Card.Text>
+                                    <Card.Text>Expert: {mushroom.experts}</Card.Text>
+                                    {isAuthenticated() ? <Button className="MushroomButton" onClick={() => console.log(mushroom._id)}>Update</Button> : null}
+                                    {isAuthenticated() ? <Button className="MushroomButton" onClick={() => this.deleteMushrooms(mushroom._id)}>Delete</Button> : null}
                                 </Card.Body>
                             </Card>
                         </Col>
