656dd7b213a6fdc224d4631bffc61c1bb02d973c
Jin Byoun <Jin.Byoun@LibertyMutual.com>
2021-11-18 13:23:22 -0800

    fixed merge

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -6,11 +6,7 @@ import RecipeGridCards from "../../components/gridcards/RecipeGridCards";
 function Recipe(props) {
   return (
     <div className="Recipe">
-<<<<<<< HEAD
-      <Header isAuthenticated={isAuthenticated} />
-=======
       <Header isAuthenticated={isAuthenticated()}/>
->>>>>>> fb1d1d3863df2f62c77389bd5bc80bf6fe0f0507
       <RecipeGridCards />
     </div>
   );
